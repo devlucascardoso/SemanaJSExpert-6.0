7629f7715b8d5d2b681340682f155378d8bf6416
Lucas Cardoso <lucascardoso946@gmail.com>
2022-03-17 21:52:52 -0300

    feat: processing audio on demand

--- server/config.js
+++ server/config.js
@@ -4,14 +4,14 @@ const currentDir = dirname(fileURLToPath(import.meta.url));
 const root = join(currentDir, "../");
 const audioDirectory = join(root, "audio");
 const publicDirectory = join(root, "public");
-
+const songsDirectory = join(audioDirectory, "songs");
 export default {
   port: process.env.PORT || 3000,
   dir: {
     root,
-    audioDirectory,
     publicDirectory,
-    songsDirectory: join(audioDirectory, "songs"),
+    audioDirectory,
+    songsDirectory,
     fxDirectory: join(audioDirectory, "fx"),
   },
   pages: {
@@ -27,5 +27,10 @@ export default {
       ".css": "text/css",
       ".js": "text/javascript",
     },
+    audioMediaType: "mp3",
+    songVolume: "0.99",
+    fallbackBitRate: "128000",
+    bitRateDivisor: 8,
+    englishConversation: join(songsDirectory, "conversation.mp3"),
   },
 };
--- server/routes.js
+++ server/routes.js
@@ -6,6 +6,7 @@ const {
   pages: { homeHTML, controllerHTML },
   constants: { CONTENT_TYPE },
 } = config;
+import { once } from "events";
 const controller = new Controller();
 async function routes(request, response) {
   const { method, url } = request;
@@ -38,6 +39,24 @@ async function routes(request, response) {
     // })
 
     return stream.pipe(response);
+  }
+
+  if (method === "GET" && url.includes("/stream")) {
+    const { stream, onClose } = controller.createClientStream();
+    request.once("close", onClose);
+    response.writeHead(200, {
+      "Content-Type": "audio/mpeg",
+      "Accept-Rages": "bytes",
+    });
+
+    return stream.pipe(response);
+  }
+
+  if (method === "POST" && url === "/controller") {
+    const data = await once(request, "data");
+    const item = JSON.parse(data);
+    const result = await controller.handleCommand(item);
+    return response.end(JSON.stringify(result));
   }
 
   //files
